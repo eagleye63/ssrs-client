a223c779853e63c9cc6f8fb2c5f920b552f27688
Pankil Panchal <panch.pankil99@gmail.com>
2018-12-22 04:29:31 +0530

    [added] partial radio button functionality in orderForm

--- src/components/service/MultiSelectDropDown.js
+++ src/components/service/MultiSelectDropDown.js
@@ -1,10 +1,48 @@
 import React, {Component} from 'react';
 import _ from "lodash";
-
+import $ from 'jquery';
+var chk = document.getElementsByTagName('input');
 class MultiSelectDropDown extends Component {
+    state = {
+        ck: 0,
+        parameters: ''
+
+    }
+   
+
+   
+    xyz = (props) => {
+    console.log(props);
+    var chklength = chk.length; 
+    chk[props].checked=true;
+    console.log(chk[props].checked);
+    for(var k=0;k< chklength;k++){
+        if(chk[k].id != props){
+            chk[k].checked =false;
+        }
+    }
+    
+    
+    console.log(chk[props].value);
+    
+    }
+    componentDidUpdate () {
+        var chklength = chk.length; 
+        for(var k=0;k< chklength;k++){
+            if( chk[k].checked === true){
+                break;
+            }
+            alert('pp');
+        }
+    }
     render() {
+        var xc=0;
+        console.log(chk);
+        
         return (
+           
             <div className="form-group">
+             
                 <label>{this.props.label}</label>
                 <div className="dropdown">
                     <div className="btn dropdown-toggle form-control" type="button"
@@ -16,22 +54,33 @@ class MultiSelectDropDown extends Component {
                                 _.map(this.props.options, (o, index) => {
                                     return (
                                         <li key={o._id}
-                                            className="dropdown-item checkbox p-0">
+                                            className="dropdown-item checkbox p-0 ">
                                             <label data-toggle="tooltip"
                                                    title={o.description}
                                                    className={'checkbox-cstm pl-4 pt-2 pb-2'}
                                                    style={{"width": '100%', "minHeight": '26px'}}>
-                                                <input onClick={this.props.handleOptionChange}
+                                                   
+                                                <input onClick={() => this.xyz(index+1)}
                                                        data-index={index}
-                                                       type="checkbox"/>
+                                                       type="checkbox"
+                                                       id={index+1}
+                                                       value={o.name}
+                                                      
+                                                       />
+                                
                                                 {o.name + " (₹" + o.baseCharge + ")"}
+                                               
                                             </label>
                                         </li>)
                                 })
                             }
+                            
                         </form>
+                        
+                        
                     </ul>
                 </div>
+            
             </div>
         );
     }
